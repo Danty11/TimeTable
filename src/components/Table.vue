65bff94cd86e2c94d027235e10c119c34d878a01
Danty11 <ambody2004@gmail.com>
2024-10-11 18:47:11 +0300

    height change

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -181,7 +181,7 @@ import { useTableStore } from '../stores/store';
 /* For mobile screens, scale down the table and content */
 @media (max-width: 500px) {
   .table-wrapper {
-    height: 240px;
+    height: 250px;
   width: 145%;
   display: block;
 }
