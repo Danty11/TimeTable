a22155472305454fed90de27078ac251ad9b1bba
Danty11 <ambody2004@gmail.com>
2024-10-18 21:41:49 +0300

    removed toast from axios

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
-import { useToast } from 'vue-toastification'
 
-const toast = useToast()
 export const baseURL = 'https://k80sowk80c808s4cogk0woc0.158.220.126.158.sslip.io/'
 const axiosIns = axios.create({
     baseURL: `${baseURL}api/`,
