b789ea2000e6d1a28e851dd9356e5f1198bba57f
Danty11 <ambody2004@gmail.com>
2025-03-14 02:18:15 +0300

    removed some unUsed variables

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,22 +1,21 @@
 <script setup lang="ts">
 import Table from '@/components/Table.vue';
 import Notes from '@/components/Notes.vue';
-import { useTableStore } from '../stores/store';
 
-const tableStore = useTableStore()
+
+
 
 onMounted(async() => {
   if(!localStorage.getItem('accessToken'))
   window.location.assign("/login")
-document.title = "Time Table"
-
+  document.title = "Time Table"
 
 })
 
 
 
 
-  const height = ref(window.innerHeight) 
+
 
 
 </script>
